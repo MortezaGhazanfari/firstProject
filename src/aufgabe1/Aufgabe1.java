8c3c8c42930ee052f3d40cd08408a483c609c8e0
MortezaGhazanfari <morteza.gh11@gmail.com>
2023-03-08 13:26:05 +0100

    datentypen

--- src/aufgabe1/Aufgabe1.java
+++ src/aufgabe1/Aufgabe1.java
@@ -1,5 +1,15 @@
 package aufgabe1;
 
 public class Aufgabe1 {
-
+    int a = 5;
+    double b = 5.0;
+    long c = 4L;
+    int d = 3+9;
+    double e = 2 + 6.0;
+    float f = 5 + 3.0f;
+    double g = 3.0d + 5.0f;
+    long h = 6L*3;
+    int i = 7/3;
+    double j = 9 / 2.0;
+    int k =- 2;
 }
